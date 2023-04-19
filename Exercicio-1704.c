ec87df1b5d707bd67d93b3c5b2f78576f0ca9002
Artur.H.I <117451939+Weaving-Instinct@users.noreply.github.com>
2023-04-19 11:02:26 -0300

    Update Exercicio-1704.c
    Edição 19/04
    
    Falta Merge part

--- Exercicio-1704.c
+++ Exercicio-1704.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdio.h>
 
-int quant, clock;
+int quant, clock, EndArray, StartArray;
 float *vetor, numeros[2], *vetorB, arrayB[2], temp;
 
-void CopyArray (){
+void CopyArray (numeros[], arrayB[], clock, quant){
 
    for (clock=0;clock<quant;clock++){
       arrayB[clock] == numeros[clock];
@@ -12,6 +12,27 @@ void CopyArray (){
    }
 
 }
+
+void SplitTopDown(A[], StartArray, EndArray, B[]){
+
+int MidArray;
+
+
+    if (EndArray-StartArray<=1){
+        return;
+    }
+
+    MidArray = (StartArray+EndArray)/2;
+
+    SplitTopDown(A[], StartArray, MidArray, B[]);
+    SplitTopDown(A[], MidArray, EndArray, B[]);
+
+
+    MergeTopDown(A[], StartArray, MidArray, EndArray, B[]);
+
+}
+
+void MergeTopDown(){}
 
 int main (){
     vetor = &numeros[2];
@@ -22,6 +43,9 @@ int main (){
     vetor = (float*)malloc(sizeof(float)*quant);
     vetorB = (float*)malloc(sizeof(float)*quant);
 
+CopyArray(numeros[], arrayB[], 0, quant);
+
+SplitTopDown(numeros[], 0, quant, arrayB[]);
     
     
 }
